dd924c2f108f9737b318497566be6c5612b55bda
vakabunga <shesterov@gmail.com>
2024-02-03 21:37:30 +0300

    finish backend for numbers sum

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const querystring = require('node:querystring');
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  params = querystring.parse((String(req.url).slice(2)));
+
+  const sum = +params.first + +params.second;
+
+  res.send(String(sum));
+
+  res.end();
+})
+
+app.listen(port, () => {
+  console.log(`Сервер запущен и слушает порт ${port}`);
+})
